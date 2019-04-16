f79047e230a76fefaf1e3383f1d55e85d6f936ae
Grant H <gharding@gmail.com>
2019-04-16 20:31:32 +0000

    added frontend

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import Sequelize from 'sequelize';
 import express from 'express';
 import session from 'express-session';
 import graphqlHTTP from 'express-graphql';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router';
+import MyPage from './pages/MyPage';
 
 import schema from './schema';
 
@@ -34,6 +38,19 @@ app.use(
     graphiql: true,
   }),
 );
+
+app.get("/", (req, res) => {
+	        const context = {};
+	        res.write("<!DOCTYPE html><html><head><title>My Page</title></head><body>");
+	        res.write("<div id='content'>");
+	        res.write(ReactDOMServer.renderToString(
+			                <StaticRouter location={req.url} context={context}>
+			                        <MyPage/>
+			                </StaticRouter>
+			        ));
+	        res.write("</div></body></html>");
+	        res.end();
+});
 
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
