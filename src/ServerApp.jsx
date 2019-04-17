243dce230d82296dd0bbd7aa4c7569d2d10964f2
Grant Harding <gharding@gmail.com>
2019-04-17 14:10:46 -0400

    add mui theme step 1

--- src/ServerApp.jsx
+++ src/ServerApp.jsx
@@ -2,13 +2,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticRouter } from 'react-router';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Routes from './routes';
 
-export default function ServerApp({ req, context }) {
+const theme = createMuiTheme({});
+
+function ServerApp({ req, context }) {
   return (
-    <StaticRouter location={req.url} context={context}>
-      <Routes />
-    </StaticRouter>
+    <MuiThemeProvider
+      theme={theme}
+    >
+      <CssBaseline />
+      <StaticRouter location={req.url} context={context}>
+        <Routes />
+      </StaticRouter>
+    </MuiThemeProvider>
   );
 }
 
@@ -16,3 +25,5 @@ ServerApp.propTypes = {
   req: PropTypes.object.isRequired,
   context: PropTypes.object.isRequired,
 };
+
+export default ServerApp;
