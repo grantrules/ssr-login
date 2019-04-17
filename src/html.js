99ad10b467cd928e35e41fe7d408a68f08175af8
Grant H <gharding@gmail.com>
2019-04-17 00:40:23 +0000

    move base html to template

---
+++ src/html.js
@@ -0,0 +1,2 @@
+export default app => `<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>My Page</title><link re l="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"></head><body><div id='content'>${app}
+</div></body></html>`
