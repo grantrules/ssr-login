239e14805bd1ca3683b75c541c559dc6d9bb633a
Grant H <gharding@gmail.com>
2019-04-19 00:07:59 +0000

    remove jss on client load

--- src/ClientApp.jsx
+++ src/ClientApp.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
 import { BrowserRouter } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
@@ -8,17 +7,26 @@ import Routes from './routes';
 
 const theme = createMuiTheme({});
 
-function ClientApp() {
-  return (
-    <MuiThemeProvider
-      theme={theme}
-    >
-      <CssBaseline />
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </MuiThemeProvider>
-  );
+class ClientApp extends React.Component {
+  componentDidMount() {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider
+        theme={theme}
+      >
+        <CssBaseline />
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 ReactDOM.hydrate(<ClientApp />,
