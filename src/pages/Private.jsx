27040bd3245aca4d670fa5024017d0c5c30b79c3
Grant Harding <gharding@gmail.com>
2019-04-24 13:43:09 -0400

    works on frontend, not on backend!

--- src/pages/Private.jsx
+++ src/pages/Private.jsx
@@ -1,3 +1,20 @@
 import React from 'react';
 
-export default () => (<div>Don’t look at me!</div>);
+import { useQuery } from 'graphql-hooks';
+
+const HOMEPAGE_QUERY = 'query { hi(name:"bill") }';
+
+function MyComponent() {
+  const { loading, error, data } = useQuery(HOMEPAGE_QUERY, {});
+
+  if (loading) return 'Loading...';
+  if (error) return 'Something Bad Happened';
+
+  return (
+    <h1>
+      {data.hi}
+    </h1>
+  );
+}
+
+export default MyComponent;
