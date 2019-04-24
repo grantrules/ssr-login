14214050c0db88b8db5c87bbeaaa5c5a14d09fae
Grant Harding <gharding@gmail.com>
2019-04-24 13:39:44 -0400

    move stuff out of index

--- src/ServerApp.jsx
+++ src/ServerApp.jsx
@@ -3,27 +3,58 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticRouter } from 'react-router';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { ClientContext } from 'graphql-hooks';
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
+import red from '@material-ui/core/colors/red';
+import JssProvider from 'react-jss/lib/JssProvider';
 import Routes from './routes';
 
-const theme = createMuiTheme({});
 
-function ServerApp({ req, context }) {
+// Create a sheetsManager instance.
+const sheetsManager = new Map();
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+    accent: red,
+    type: 'light',
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
+const generateClassName = createGenerateClassName();
+
+
+const context = {};
+
+function ServerApp({ req, client, sheetsRegistry }) {
   return (
-    <MuiThemeProvider
-      theme={theme}
-    >
-      <CssBaseline />
-      <StaticRouter location={req.url} context={context}>
-        <Routes />
-      </StaticRouter>
-    </MuiThemeProvider>
+    <ClientContext.Provider value={client}>
+      <JssProvider registry={sheetsRegistry} generateClassName={generateClassName}>
+        <MuiThemeProvider theme={theme} sheetsManager={sheetsManager}>
+
+          <CssBaseline />
+          <StaticRouter location={req.url} context={context}>
+            <Routes />
+          </StaticRouter>
+        </MuiThemeProvider>
+      </JssProvider>
+    </ClientContext.Provider>
   );
 }
 
 ServerApp.propTypes = {
   req: PropTypes.object.isRequired,
-  context: PropTypes.object.isRequired,
+  client: PropTypes.object.isRequired,
+  sheetsRegistry: PropTypes.object.isRequired,
 };
 
 export default ServerApp;
--- src/index.jsx
+++ src/index.jsx
@@ -5,18 +5,9 @@ import graphqlHTTP from 'express-graphql';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { SheetsRegistry } from 'jss';
-import JssProvider from 'react-jss/lib/JssProvider';
-import {
-  MuiThemeProvider,
-  createMuiTheme,
-  createGenerateClassName,
-} from '@material-ui/core/styles';
-import green from '@material-ui/core/colors/green';
-import red from '@material-ui/core/colors/red';
-
-import graphql from 'graphql';
-import { GraphQLClient, ClientContext } from 'graphql-hooks';
 import memCache from 'graphql-hooks-memcache';
+import { graphql } from 'graphql';
+import { GraphQLClient } from 'graphql-hooks';
 import { getInitialState } from 'graphql-hooks-ssr';
 
 import ServerApp from './ServerApp';
@@ -43,9 +34,10 @@ sequelize
   });
 
 const notFetch = (_, { body }) => {
-  const result = { ok: true, json: () => Promise.resolve(this.data).then(JSON.parse), data: {} };
+  const { query } = JSON.parse(body);
+  const result = { ok: true, json() { return Promise.resolve(this.data); }, data: {} };
 
-  return graphql(schema, body).then((res) => { result.data = res; return result; });
+  return graphql(schema, query).then((res) => { result.data = res; return result; });
 };
 
 
@@ -71,40 +63,15 @@ app.use(
 );
 
 app.get('/*', async (req, res) => {
-  const sheetsRegistry = new SheetsRegistry();
-
-  // Create a sheetsManager instance.
-  const sheetsManager = new Map();
-
-  // Create a theme instance.
-  const theme = createMuiTheme({
-    palette: {
-      primary: green,
-      accent: red,
-      type: 'light',
-    },
-    typography: {
-      useNextVariants: true,
-    },
-  });
-
-  const generateClassName = createGenerateClassName();
   const client = new GraphQLClient({
     url: '/hey',
     cache: memCache(),
     fetch: notFetch,
   });
-
-  const context = {};
+  const sheetsRegistry = new SheetsRegistry();
 
   const App = () => (
-    <ClientContext.Provider value={client}>
-      <JssProvider registry={sheetsRegistry} generateClassName={generateClassName}>
-        <MuiThemeProvider theme={theme} sheetsManager={sheetsManager}>
-          <ServerApp req={req} context={context} />
-        </MuiThemeProvider>
-      </JssProvider>
-    </ClientContext.Provider>
+    <ServerApp client={client} req={req} sheetsRegistry={sheetsRegistry} />
   );
 
   const initialState = await getInitialState({ App, client });
