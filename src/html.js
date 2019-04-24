4eb8e8ac2bd14a1bbff22756e382e1efac688459
Grant Harding <gharding@gmail.com>
2019-04-24 02:23:52 -0400

    build client bundle with hash in filename

--- src/html.js
+++ src/html.js
@@ -1,2 +1,4 @@
+import scripts from './manifest.json';
+
 export default (app, css, initialState) => `<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>My Page</title><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"></head><body><div id='content'>${app}
-</div><style id="jss-server-side">${css}</style><script>window.__INITIAL_STATE__=${JSON.stringify(initialState).replace(/</g, '\\u003c')}</script><script src="/static/client.bundle.js" async></script></body></html>`;
+</div><style id="jss-server-side">${css}</style><script>window.__INITIAL_STATE__=${JSON.stringify(initialState).replace(/</g, '\\u003c')}</script><script src="${scripts['main.js']}" async></script></body></html>`;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const WebpackManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
   entry: './build/ClientApp.js',
   output: {
+    publicPath: '/static/',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'client.bundle.js',
+    filename: 'client.[hash].js',
   },
+  plugins: [
+    new WebpackManifestPlugin({
+      fileName: '../build/manifest.json',
+    }),
+  ],
   mode: 'development',
 };
